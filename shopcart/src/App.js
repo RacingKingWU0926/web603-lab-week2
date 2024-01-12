36b383ad793d3bb222541f0ca04e32274c8fa81f
JasonLei <jlei@activision.com>
2024-01-11 21:19:11 -0800

    get app running with items

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -1,25 +1,64 @@
+import React, { Component } from "react";
+
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { products } from "./products";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: products
+    }
+  }
+
+  renderProducts(products) {
+    return (
+      <div>
+        {products.map(product => 
+          <div key={product.id}>
+            <p>{product.name}</p>
+            <img src={product.image}/>
+            <span>quantities: {product.quantity}</span>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="shop_cart">
+        <h1>Shop to React</h1>
+        {this.renderProducts(this.state.products)}
+      </div>
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ shopcart/src/products.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 0,
+    name: "Unisex Cologne",
+    image: "./products/cologne.jpg",
+    quantity: 0
+  },
+  {
+    id: 1,
+    name: "Apple iWatch",
+    image: "./products/iwatch.jpg",
+    quantity: 0
+  },
+  {
+    id: 2,
+    name: "Unique Mug",
+    image: "./products/mug.jpg",
+    quantity: 0
+  },
+  {
+    id: 3,
+    name: "Mens Wallet",
+    image: "./products/wallet.jpg",
+    quantity: 0
+  }
+];
