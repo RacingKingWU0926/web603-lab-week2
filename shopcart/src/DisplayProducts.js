dafb32a3116efade122c9e88073e8e5609e1a919
JasonLei <jlei@activision.com>
2024-02-04 19:39:55 -0800

    add modal

--- shopcart/src/DisplayProducts.js
+++ shopcart/src/DisplayProducts.js
@@ -1,7 +1,15 @@
-// DisplayProducts.js
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 
 const DisplayProducts = ({ products, onQuantityChange }) => {
+  // Initialize with null since no product is selected initially
+  const [activeProductId, setActiveProductId] = useState(null);
+
+  // Function to show modal for a specific product
+  const handleShow = (productId) => setActiveProductId(productId);
+  // Function to close the modal
+  const handleClose = () => setActiveProductId(null);
+
   return (
     <div>
       {products.map((product) => (
@@ -12,6 +20,8 @@ const DisplayProducts = ({ products, onQuantityChange }) => {
               className="product-image"
               src={product.image}
               alt={product.name}
+              onClick={() => handleShow(product.id)} // Pass the product's id instead of just setting show to true
+              style={{ cursor: "pointer" }}
             />
           </div>
           <div className="col quantity-input-container">
@@ -21,10 +31,28 @@ const DisplayProducts = ({ products, onQuantityChange }) => {
               min="0"
               max="100"
               value={product.quantity}
-              onChange={(e) => onQuantityChange(product.id, e.target.value)}
+              onChange={(e) =>
+                onQuantityChange(product.id, parseInt(e.target.value))
+              }
             />
             <span> quantity</span>
           </div>
+          {activeProductId === product.id && ( // Check if this product's modal should be shown
+            <Modal show={activeProductId !== null} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>{product.name}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <img
+                  src={product.image}
+                  width="350"
+                  alt={product.name}
+                  className="mx-auto d-block"
+                />
+                <p>Rating: {product.rating}</p>
+              </Modal.Body>
+            </Modal>
+          )}
         </div>
       ))}
     </div>
